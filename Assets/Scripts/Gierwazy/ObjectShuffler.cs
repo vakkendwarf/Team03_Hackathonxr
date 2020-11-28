180cd59ee9e8ad35c487a68147a9e681ddff361a
Maciej Gierwatowski <51367363+Maciej-Gierwatowski@users.noreply.github.com>
2020-11-28 21:58:00 +0100

    ObjectRandomizer

---
+++ Assets/Scripts/Gierwazy/ObjectShuffler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectShuffler : MonoBehaviour
+{
+
+    [SerializeField]
+    List<GameObject> objectsToShuffle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject obj in objectsToShuffle) {
+            var spawnBox = transform.localScale;
+            Vector3 position = new Vector3(Random.value * spawnBox.x, Random.value * spawnBox.x, Random.value * spawnBox.x);
+            position = transform.TransformPoint(position - spawnBox / 2);
+            obj.transform.position = position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
