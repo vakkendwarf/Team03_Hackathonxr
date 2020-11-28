dfe4fbdb3e8f73fb3847a117e4c24e103d90706a
KKovs <krzysztof.kowalski.kontakt@gmail.com>
2020-11-28 15:23:48 +0100

    Kitchen scripts PoC

---
+++ Assets/Kitchen/Scripts/Tap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour
+{
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("colliding");
+
+		var waterStream = GameObject.Find("water_stream");
+		waterStream.GetComponent<MeshRenderer>().enabled = true;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Kitchen/Scripts/WaterStream.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterStream : MonoBehaviour
+{
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("colliding with water");
+
+		var dish = collision.gameObject.GetComponent<Dish>();
+		if (dish)
+		{
+			if (dish.isDirty && !dish.isCurrentlyBeingCleaned)
+			{
+				dish.isCurrentlyBeingCleaned = true;
+				StartCoroutine(CheckDistance(dish));
+			}
+		}
+	}
+
+	private IEnumerator CheckDistance(Dish dish)
+	{
+		Debug.Log($"check distance start");
+		float secs = 2;
+		var minDistance = 5f;
+		float checkTime = 0.1f;
+		for (float i = 0; i < secs; i += checkTime)
+		{
+			Debug.Log($"checking for time {i}");
+			if ((dish.gameObject.transform.position - transform.position).magnitude > minDistance)
+			{
+				dish.isCurrentlyBeingCleaned = false;
+				Debug.Log($"interrupted washing");
+				break;
+			}
+			yield return new WaitForSeconds(checkTime);
+		}
+
+		if (dish.isCurrentlyBeingCleaned)
+		{
+			Debug.Log($"cleaned dish");
+			// successfully cleaned dish
+			dish.CleanUpDish();
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
