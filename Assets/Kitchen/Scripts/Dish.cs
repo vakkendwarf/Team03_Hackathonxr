89814396aefb922da2e7c4a24bf676d1e3cb297b
KKovs <krzysztof.kowalski.kontakt@gmail.com>
2020-11-28 15:23:48 +0100

    Kitchen scripts PoC

---
+++ Assets/Kitchen/Scripts/Dish.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dish : MonoBehaviour
+{
+	public bool isDirty;
+
+	public Material cleanDishMaterial;
+	public Material dirtyDishMaterial;
+	public bool isCurrentlyBeingCleaned;
+
+	public void CleanUpDish()
+	{
+		if (cleanDishMaterial)
+		{
+			GetComponent<Renderer>().material = cleanDishMaterial;
+			isDirty = false;
+		}
+	}
+
+
+    // Start is called before the first frame update
+    void Start()
+	{
+		if (cleanDishMaterial)
+		{
+			GetComponent<Renderer>().material = dirtyDishMaterial;
+			isDirty = true;
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Kitchen/Scripts/WaterStream.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterStream : MonoBehaviour
+{
+	public float cleaningTimeSec = 2f;
+	public float cleaningDistance = 5f;
+
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("colliding with water");
+
+		var dish = collision.gameObject.GetComponent<Dish>();
+		if (dish)
+		{
+			if (dish.isDirty && !dish.isCurrentlyBeingCleaned)
+			{
+				dish.isCurrentlyBeingCleaned = true;
+				StartCoroutine(CheckDistance(dish));
+			}
+		}
+	}
+
+	private IEnumerator CheckDistance(Dish dish)
+	{
+		Debug.Log($"check distance start");
+		float checkTime = 0.1f;
+		for (float i = 0; i < cleaningTimeSec; i += checkTime)
+		{
+			var dist = (dish.gameObject.transform.position - transform.position).magnitude;
+			Debug.Log($"checking for time {i} distance {dist} ");
+			if (dist > cleaningDistance)
+			{
+				dish.isCurrentlyBeingCleaned = false;
+				Debug.Log($"interrupted washing");
+				break;
+			}
+			yield return new WaitForSeconds(checkTime);
+		}
+
+		if (dish.isCurrentlyBeingCleaned)
+		{
+			Debug.Log($"cleaned dish");
+			// successfully cleaned dish
+			dish.CleanUpDish();
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
