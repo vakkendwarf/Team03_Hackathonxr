97bedc518c9dd1578b002f779c8c129f7e3c48d3
GRostek <46569840+GRostek@users.noreply.github.com>
2020-11-28 18:55:20 +0100

    Working dish and sink

---
+++ Assets/Kitchen/Scripts/Tap.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour
+{
+	public ParticleSystem particleSystem;
+	public bool opened;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log($"colliding, state {opened}");
+
+		Switch(); // should be invoked by hand in VR
+	}
+
+	public void Switch()
+	{
+		if (!opened)
+		{
+			Open();
+		}
+		else
+		{
+			Close();
+		}
+
+		opened = !opened;
+	}
+
+	public void Open()
+	{
+		var waterStream = GameObject.Find("water_stream");
+		waterStream.GetComponent<MeshRenderer>().enabled = true;
+		waterStream.GetComponent<BoxCollider>().enabled = true;
+		particleSystem.Play();
+	}
+
+	public void Close()
+	{
+		var waterStream = GameObject.Find("water_stream");
+		waterStream.GetComponent<MeshRenderer>().enabled = false;
+		waterStream.GetComponent<BoxCollider>().enabled = false;
+		particleSystem.Stop();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		particleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
