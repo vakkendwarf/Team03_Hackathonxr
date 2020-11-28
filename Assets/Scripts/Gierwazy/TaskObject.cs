f47b31f65f13800077b676270f937f3952487371
Maciej Gierwatowski <51367363+Maciej-Gierwatowski@users.noreply.github.com>
2020-11-28 17:35:38 +0100

    Easier to stack on enable/disable objects

---
+++ Assets/Scripts/Gierwazy/TaskObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskObject : MonoBehaviour 
+{
+    public GameObject sibling;
+
+   
+    private void OnDisable() {
+        sibling.SetActive(true);
+        //sibling.GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
