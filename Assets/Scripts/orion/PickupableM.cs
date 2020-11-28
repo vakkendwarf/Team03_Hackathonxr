52d616ec79aa983d17de4d5a3d70ac17b00e4bff
Mikołaj Kierski <mikolaj.kierski@outlook.com>
2020-11-28 14:41:08 +0100

    1

---
+++ Assets/Scripts/orion/PickupableM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupableM : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject itemStorageToDeliver;
+    public t_Task task;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
