7750e926b01c9c2fa23269ba1a2774e0717d7e66
Maciej Gierwatowski <51367363+Maciej-Gierwatowski@users.noreply.github.com>
2020-11-29 10:32:53 +0100

    functional kitchen

---
+++ Assets/Dirt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dirt : MonoBehaviour
+{
+
+    public GameObject dish;
+    private void Start() {
+        dish = transform.parent.gameObject;
+    }
+
+    public void TouchedBySponge(bool value) {
+        if (dish.GetComponent<Dish>().isUnderSink)
+            Destroy(gameObject);
+        //dish.GetComponent<Dish>().DirtRemoving(value);
+    }
+    
+}
--- Assets/Kitchen/Scripts/Dish.cs
+++ Assets/Kitchen/Scripts/Dish.cs
@@ -4,10 +4,55 @@ using UnityEngine;
 
 public class Dish : MonoBehaviour
 {
+
 	public bool isDirty;
 
 	public bool isUnderSink;
 	public bool isCurrentlyBeingCleaned;
+
+	public bool isDone;
+
+
+	public void CleanUpDish() {
+		Destroy(transform.GetChild(0).gameObject);
+	}
+
+
+	// Start is called before the first frame update
+	void Start() {
+		isDirty = true;
+	}
+
+	// Update is called once per frame
+	void Update() {
+	}
+
+	// leagcy
+	public void SetIsUnderSink(bool value) {
+		if (value) {
+			isUnderSink = true;
+			isCurrentlyBeingCleaned = true;
+		}
+		else {
+			isUnderSink = false;
+			isCurrentlyBeingCleaned = false;
+		}
+	}
+
+
+
+	// Wersja z obsługą czasu szurowanie brudu... debuger mi się na tym wywala, wiec uproscilem. Gierwazy
+	/*public bool isDirty;
+
+	public bool isUnderSink;
+	public bool isCurrentlyBeingCleaned;
+
+	public float timeToClean = 4f;
+
+	private float timeCleanedTemp;
+	private float timeCleaned;
+
+	private GameObject dirt;
 
 
 	public void CleanUpDish()
@@ -25,7 +70,15 @@ public class Dish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+		if (isCurrentlyBeingCleaned) {
+			if (timeCleaned + Time.time - timeCleanedTemp > timeToClean) {
+				CleanUpDish();
+            }
+        } else {
+			if (timeCleaned > timeToClean) {
+				CleanUpDish();
+			}
+        }
     }
 
 	public void SetIsUnderSink(bool value) {
@@ -36,5 +89,26 @@ public class Dish : MonoBehaviour
 			isCurrentlyBeingCleaned = false;
         }
     }
+
+	public void SetCurrentlyCleaned(bool value) {
+		if (value && !isCurrentlyBeingCleaned) {
+			isCurrentlyBeingCleaned = true;
+			timeCleanedTemp = Time.time;
+        } 
+		if (!value) {
+			isCurrentlyBeingCleaned = false;
+			timeCleaned = Time.time - timeCleanedTemp;
+		}
+    }
+
+	public void DirtRemoving(bool value) {
+		if (value) {
+			if (isUnderSink) {
+				SetCurrentlyCleaned(true);
+			}
+		} else {
+			SetCurrentlyCleaned(false);
+        }
+    }*/
 
 }
